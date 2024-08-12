d274282637c5263cca4ade9cb8ace5440a07b374
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-08-12 19:17:13 +0200

    Adding all html refernces.

---
+++ script.js
@@ -0,0 +1,16 @@
+// Arrays
+const myLibrary = [];
+
+// Buttons
+const newBtn = document.querySelector(".new-btn");
+const addBtn = document.querySelector(".add-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+// Dialog modal
+const modal = document.querySelector(".modal");
+
+// Inputs
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const nopInput = document.getElementById("nop");
+const readInput = document.getElementById("read");
